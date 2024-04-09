8cf6c937ab8b83ffff0ce673c83e51be7df6b090
k-colish <k-colish@onu.edu>
2024-04-09 08:11:29 -0400

    Update Destinations.java

--- src/MP3Package/Destinations.java
+++ src/MP3Package/Destinations.java
@@ -3,13 +3,13 @@ package MP3Package;
 public class Destinations {
 	private int distance;
 	private String name;
-	private boolean isFort;
+	private boolean hasStore;
 	
 	
-	public Destinations(int distance, String name, boolean isFort) {
+	public Destinations(int distance, String name, boolean hasStore) {
 		this.distance = distance;
 		this.name = name;
-		this.isFort = isFort;
+		this.hasStore = hasStore;
 	}
 	
 	public int getDistance() {
