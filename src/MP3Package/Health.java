6e1e343d1da5676177707c2672b718ac0a290ad5
JustinSchiefer <143095431+JustinSchiefer@users.noreply.github.com>
2024-04-23 14:02:12 -0400

    Start of people & health

--- src/MP3Package/Health.java
+++ src/MP3Package/Health.java
@@ -1,5 +1,23 @@
 package MP3Package;
 
+import java.lang.reflect.Array;
+
 public class Health {
+//Need, Wagon, Weather, random, river, people,
+    private Array condition;
+
+    Health(Array condition) {
+
+        this.condition = condition;
+    }
+
+   // public Array getCondition() {
+       // return condition;
+  //  }
+
+    //public Array setCondition(String condition, int x)
+    //{
+       // condition.set(x) = condition;
+   // }
 
 }
--- src/MP3Package/People.java
+++ src/MP3Package/People.java
@@ -1,4 +1,13 @@
 package MP3Package;
 
+import java.util.ArrayList;
+
 public class People {
+    private final ArrayList<String> names = new ArrayList<String>();
+
+    People()
+    {
+
+    }
+
 }
